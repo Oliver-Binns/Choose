b0b85f6d5230407a274555eae85a7e72f307911b
Oliver Binns <mail@oliverbinns.co.uk>
2024-06-16 02:26:56 +0100

    feat: added extra details to onboarding view

---
+++ GeneralElection2017/OnboardingPage.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct OnboardingPage: View {
+    let imageName: String
+    let text: String
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 100)
+                .foregroundColor(.secondary)
+            
+            Text(text)
+                .font(.headline)
+                .multilineTextAlignment(.center)
+        }.padding()
+    }
+}
+
+#Preview {
+    OnboardingPage(
+        imageName: "location.fill.viewfinder",
+        text: "Select your constituency to get information relevant to your area."
+    )
+}
--- GeneralElection2017/OnboardingView.swift
+++ GeneralElection2017/OnboardingView.swift
@@ -14,11 +14,35 @@ struct OnboardingView: View {
                 .font(.title)
                 .fontWeight(.bold)
             
-            Text("June 4th")
+            Label("June 4th", systemImage: "calendar")
                 .font(.title3)
                 .fontWeight(.medium)
             
-            Spacer()
+            TabView {
+                OnboardingPage(
+                    imageName: "location.fill.viewfinder",
+                    text: "Select your constituency to get information relevant to your area."
+                )
+                
+                OnboardingPage(
+                    imageName: "hand.draw",
+                    text: "Swipe right on policies you agree with. Swipe left on those you don't."
+                )
+                
+                OnboardingPage(
+                    imageName: "person.crop.circle.badge.questionmark.fill",
+                    text: "Find out which parties you agree with, and who their candidates are."
+                )
+                
+                OnboardingPage(
+                    imageName: "chart.pie.fill",
+                    text: "Find out which policies areas you agree and disagree on."
+                )
+            }
+            .background(.ultraThinMaterial)
+            .clipShape(.rect(cornerRadius: 8))
+            .padding()
+            .tabViewStyle(.page)
             
             Button {
                 withAnimation {
