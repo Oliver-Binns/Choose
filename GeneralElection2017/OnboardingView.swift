63f7cf2f29e9a54e25ab478c0cbb884f2959142b
Oliver Binns <mail@oliverbinns.co.uk>
2024-06-16 11:51:31 +0100

    feat: added new onboarding screen for first-time users (#6)
    * feat: added initial onboarding view design
    
    * feat: added extra details to onboarding view
    
    * chore: fixed linting errors

--- GeneralElection2017/OnboardingView.swift
+++ GeneralElection2017/OnboardingView.swift
@@ -1,9 +1,63 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @AppStorage("OnboardingComplete")
+    private var onboardingComplete: Bool?
+
     var body: some View {
-        VStack {
+        VStack(spacing: 4) {
+            Image("Ballot")
+                .resizable()
+                .frame(maxWidth: 200, maxHeight: 200)
+
+            Text("General Election 24")
+                .font(.title)
+                .fontWeight(.bold)
+
+            Label("June 4th", systemImage: "calendar")
+                .font(.title3)
+                .fontWeight(.medium)
+
+            TabView {
+                OnboardingPage(
+                    imageName: "location.fill.viewfinder",
+                    text: "Select your constituency to get information relevant to your area."
+                )
+
+                OnboardingPage(
+                    imageName: "hand.draw",
+                    text: "Swipe right on policies you agree with. Swipe left on those you don't."
+                )
+
+                OnboardingPage(
+                    imageName: "person.crop.circle.badge.questionmark.fill",
+                    text: "Find out which parties you agree with, and who their candidates are."
+                )
+
+                OnboardingPage(
+                    imageName: "chart.pie.fill",
+                    text: "Find out which policies areas you agree and disagree on."
+                )
+            }
+            .background(.ultraThinMaterial)
+            .clipShape(.rect(cornerRadius: 8))
+            .padding()
+            .tabViewStyle(.page)
+
+            Button {
+                withAnimation {
+                    onboardingComplete = true
+                }
+            } label: {
+                Text("Continue")
+                    .padding(.vertical, 4)
+                    .frame(maxWidth: .infinity)
+            }
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .padding()
         }
+        .background(Image("Background").blur(radius: 70))
     }
 }
 
